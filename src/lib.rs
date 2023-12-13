b1dbaa9d74028d06295a0b0db92cf2742b3ad196
eshad <fidaeshad10@gmail.com>
2023-12-13 13:00:06 -0600

    removed comments

--- src/lib.rs
+++ src/lib.rs
@@ -26,21 +26,16 @@ impl Future for TimerFuture {
 }
 
 impl TimerFuture {
-    /// Create a new `TimerFuture` which will complete after the provided
-    /// timeout.
     pub fn new(duration: Duration) -> Self {
         let shared_state = Arc::new(Mutex::new(SharedState {
             completed: false,
             waker: None,
         }));
 
-        // Spawn the new thread
         let thread_shared_state = shared_state.clone();
         thread::spawn(move || {
             thread::sleep(duration);
             let mut shared_state = thread_shared_state.lock().unwrap();
-            // Signal that the timer has completed and wake up the last
-            // task on which the future was polled, if one exists.
             shared_state.completed = true;
             if let Some(waker) = shared_state.waker.take() {
                 waker.wake()
