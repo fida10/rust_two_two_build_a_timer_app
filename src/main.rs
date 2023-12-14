4d5d55e4fb8a391856bf62874f7f908a3cbf0f08
eshad <fidaeshad10@gmail.com>
2023-12-14 08:35:17 -0600

    completed the code to make async code run

---
+++ src/main.rs
@@ -0,0 +1,96 @@
+use futures::{
+    future::{BoxFuture, FutureExt},
+    task::{waker_ref, ArcWake},
+};
+use std::{
+    future::Future,
+    sync::mpsc::{sync_channel, Receiver, SyncSender},
+    sync::{Arc, Mutex},
+    task::Context,
+    time::Duration,
+};
+use two_two_build_a_timer_app::TimerFuture;
+
+struct Executor {
+    ready_queue: Receiver<Arc<Task>>,
+}
+impl Executor {
+    fn run(&self) {
+        while let Ok(task) = self.ready_queue.recv() {
+            // Take the future, and if it has not yet completed (is still Some),
+            // poll it in an attempt to complete it.
+            let mut future_slot = task.future.lock().unwrap();
+            if let Some(mut future) = future_slot.take() {
+                // Create a `LocalWaker` from the task itself
+                let waker = waker_ref(&task);
+                let context = &mut Context::from_waker(&waker);
+                // `BoxFuture<T>` is a type alias for
+                // `Pin<Box<dyn Future<Output = T> + Send + 'static>>`.
+                // We can get a `Pin<&mut dyn Future + Send + 'static>`
+                // from it by calling the `Pin::as_mut` method.
+                if future.as_mut().poll(context).is_pending() {
+                    // We're not done processing the future, so put it
+                    // back in its task to be run again in the future.
+                    *future_slot = Some(future);
+                }
+            }
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Spawner {
+    task_sender: SyncSender<Arc<Task>>,
+}
+impl Spawner {
+    fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
+        let future = future.boxed();
+        let task = Arc::new(Task {
+            future: Mutex::new(Some(future)),
+            task_sender: self.task_sender.clone(),
+        });
+        self.task_sender.send(task).expect("too many tasks queued");
+    }
+}
+
+struct Task {
+    future: Mutex<Option<BoxFuture<'static, ()>>>,
+
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+fn new_executor_and_spawner() -> (Executor, Spawner) {
+    const MAX_QUEUED_TASKS: usize = 10_000;
+    let (task_sender, ready_queue) = sync_channel(MAX_QUEUED_TASKS);
+    (Executor { ready_queue }, Spawner { task_sender })
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        let cloned = arc_self.clone();
+        arc_self
+            .task_sender
+            .send(cloned)
+            .expect("Too many tasks queued")
+    }
+}
+
+fn main() {
+    let (executor, spawner) = new_executor_and_spawner();
+
+    spawner.spawn(async {
+        println!("howdy!");
+        TimerFuture::new(Duration::new(5, 0)).await;
+        println!("done!");
+    });
+
+    spawner.spawn(async {
+        println!("hola!");
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("finito!");
+    });
+
+    drop(spawner);
+
+    executor.run();
+}
